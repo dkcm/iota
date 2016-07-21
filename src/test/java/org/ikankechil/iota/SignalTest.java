cd4017da82b80e8416c3019555a828fd736f0fce
dkcm <daniel.kuan@gmail.com>
2016-07-21 19:09:20 +0800

    Added SignalTest and enhanced SignalTimeSeriesTest.

---
+++ src/test/java/org/ikankechil/iota/SignalTest.java
@@ -0,0 +1,31 @@
+/**
+ * SignalTest.java v0.1 21 July 2016 6:46:31 pm
+ *
+ * Copyright © 2016 Daniel Kuan.  All rights reserved.
+ */
+package org.ikankechil.iota;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * JUnit test for <code>Signal</code>.
+ * <p>
+ *
+ * @author Daniel Kuan
+ * @version 0.1
+ */
+public class SignalTest {
+
+  private static final String[] SIGNALS = { "BUY", "SELL", "NONE" };
+
+  @Test
+  public void validSignals() {
+    for (final String signal : SIGNALS) {
+      assertNotNull(Signal.valueOf(signal));
+    }
+    assertEquals(SIGNALS.length, Signal.values().length);
+  }
+
+}
--- src/test/java/org/ikankechil/iota/SignalTimeSeriesTest.java
+++ src/test/java/org/ikankechil/iota/SignalTimeSeriesTest.java
@@ -7,12 +7,14 @@ package org.ikankechil.iota;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 /**
- * A JUnit test for <code>SignalTimeSeries</code>.
+ * JUnit test for <code>SignalTimeSeries</code>.
  * <p>
  *
  * @author Daniel Kuan
@@ -20,18 +22,54 @@ import org.junit.rules.ExpectedException;
  */
 public class SignalTimeSeriesTest {
 
-  private SignalTimeSeries signals;
+  private SignalTimeSeries    signals;
+
+  private static final String DATE   = "20150101";
 
   @Rule
-  public ExpectedException thrown = ExpectedException.none();
+  public ExpectedException    thrown = ExpectedException.none();
+
+  @Before
+  public void setUp() throws Exception {
+    signals = new SignalTimeSeries(null, 1);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    signals = null;
+  }
+
+  @Test
+  public final void size() {
+    assertEquals(1, signals.signals().length);
+  }
 
   @Test
   public final void setSignal() {
-    signals = new SignalTimeSeries(null, 1);
     final Signal expected = Signal.BUY;
     signals.signal(expected, 0);
 
     assertEquals(expected, signals.signal(0));
+  }
+
+  @Test
+  public final void setSignal2() {
+    final Signal expected = Signal.SELL;
+    signals.set(DATE, expected, 0);
+
+    assertEquals(expected, signals.signal(0));
+  }
+
+  @Test
+  public final void cannotSetNullSignal() {
+    thrown.expect(NullPointerException.class);
+    signals.set(DATE, null, 0);
+  }
+
+  @Test
+  public final void cannotSetNullSignal2() {
+    thrown.expect(NullPointerException.class);
+    signals.signal(null, 0);
   }
 
 }
