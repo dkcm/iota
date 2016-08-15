b6df8507cecc3eda93db4c6fd2393ae13448c6e2
dkcm <daniel.kuan@gmail.com>
2016-08-15 23:13:15 +0800

    Enhanced Chaikin Oscillator performance.

--- src/main/java/org/ikankechil/iota/indicators/volume/ChaikinOscillator.java
+++ src/main/java/org/ikankechil/iota/indicators/volume/ChaikinOscillator.java
@@ -1,5 +1,5 @@
 /**
- * ChaikinOscillator.java  v0.1  4 December 2014 3:13:53 PM
+ * ChaikinOscillator.java  v0.2  4 December 2014 3:13:53 PM
  *
  * Copyright © 2014-2016 Daniel Kuan.  All rights reserved.
  */
@@ -22,23 +22,25 @@ import com.tictactec.ta.lib.RetCode;
  * anticipate crosses into positive or negative territory by looking for bullish
  * or bearish divergences, respectively."
  * <p>
- * http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:
- * chaikin_oscillator
+ * http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:chaikin_oscillator
+ * https://www.tradingview.com/stock-charts-support/index.php/Chaikin_Oscillator
  *
  * @author Daniel Kuan
- * @version
+ * @version 0.2
  */
 public class ChaikinOscillator extends AbstractIndicator {
 
-  private final int fast;
-  private final int slow;
+  private final int       fast;
+  private final int       slow;
+
+  private static final AD AD = new AD();  // Accumulation Distribution Line
 
   public ChaikinOscillator() {
     this(THREE, TEN);
   }
 
   public ChaikinOscillator(final int fast, final int slow) {
-    super(TA_LIB.adOscLookback(fast, slow));
+    super(Math.max(fast, slow) - ONE);
     throwExceptionIfNegative(fast, slow);
 
     this.fast = fast;
@@ -52,17 +54,28 @@ public class ChaikinOscillator extends AbstractIndicator {
                             final MInteger outBegIdx,
                             final MInteger outNBElement,
                             final double[] output) {
-    return TA_LIB.adOsc(start,
-                        end,
-                        ohlcv.highs(),
-                        ohlcv.lows(),
-                        ohlcv.closes(),
-                        toDoubles(ohlcv.volumes()), // copy volumes
-                        fast,
-                        slow,
-                        outBegIdx,
-                        outNBElement,
-                        output);
+    // Formula:
+    // 1. Money Flow Multiplier = [(Close  -  Low) - (High - Close)] /(High - Low)
+    // 2. Money Flow Volume = Money Flow Multiplier x Volume for the Period
+    // 3. ADL = Previous ADL + Current Period's Money Flow Volume
+    // 4. Chaikin Oscillator = (3-day EMA of ADL) - (10-day EMA of ADL)
+
+    // compute Accumulation Distribution Line
+    final double[] ad = new double[ohlcv.size()];
+    AD.compute(start, end, ohlcv, outBegIdx, outNBElement, ad);
+
+    // compute EMAs
+    final double[] emaFast = ema(ad, fast);
+    final double[] emaSlow = ema(ad, slow);
+
+    // compute indicator
+    for (int i = ZERO, j = slow - fast; i < output.length; ++i, ++j) {
+      output[i] = emaFast[j] - emaSlow[i];
+    }
+
+    outBegIdx.value = lookback;
+    outNBElement.value = output.length;
+    return RetCode.Success;
   }
 
 }
---
+++ src/test/java/org/ikankechil/iota/indicators/volume/ChaikinOscillatorTest.java
@@ -0,0 +1,32 @@
+/**
+ * ChaikinOscillatorTest.java v0.1  15 August 2016 7:36:21 pm
+ *
+ * Copyright © 2016 Daniel Kuan.  All rights reserved.
+ */
+package org.ikankechil.iota.indicators.volume;
+
+import java.io.IOException;
+
+import org.ikankechil.iota.indicators.AbstractIndicatorTest;
+import org.junit.BeforeClass;
+
+/**
+ * JUnit test for <code>ChaikinOscillator</code>.
+ * <p>
+ *
+ * @author Daniel Kuan
+ * @version 0.1
+ */
+public class ChaikinOscillatorTest extends AbstractIndicatorTest {
+
+  public ChaikinOscillatorTest() {
+    super(9);
+  }
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws IOException {
+    TEST_CLASS = ChaikinOscillatorTest.class;
+    AbstractIndicatorTest.setUpBeforeClass();
+  }
+
+}
