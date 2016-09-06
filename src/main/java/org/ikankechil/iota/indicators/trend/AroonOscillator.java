e7ff122db720a25bbf45d168049b836d20897d10
dkcm <daniel.kuan@gmail.com>
2016-09-07 00:52:23 +0800

    Updated Aroon Oscillator indicator.

--- src/main/java/org/ikankechil/iota/indicators/trend/AroonOscillator.java
+++ src/main/java/org/ikankechil/iota/indicators/trend/AroonOscillator.java
@@ -1,5 +1,5 @@
 /**
- * AroonOscillator.java  v0.1 9 December 2014 12:04:45 PM
+ * AroonOscillator.java  v0.2  9 December 2014 12:04:45 PM
  *
  * Copyright © 2014-2016 Daniel Kuan.  All rights reserved.
  */
@@ -14,19 +14,20 @@ import com.tictactec.ta.lib.RetCode;
 /**
  * Aroon Oscillator
  * <p>
- *
+ * http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:aroon_oscillator
+ * https://www.incrediblecharts.com/indicators/aroon_oscillator.php
  *
  * @author Daniel Kuan
- * @version 0.1
+ * @version 0.2
  */
 public class AroonOscillator extends AbstractIndicator {
 
   public AroonOscillator() {
-    this(FOURTEEN);
+    this(TWENTY_FIVE);
   }
 
   public AroonOscillator(final int period) {
-    super(period, TA_LIB.aroonOscLookback(period));
+    super(period, period);
   }
 
   @Override
@@ -36,14 +37,39 @@ public class AroonOscillator extends AbstractIndicator {
                             final MInteger outBegIdx,
                             final MInteger outNBElement,
                             final double[] output) {
-    return TA_LIB.aroonOsc(start,
-                           end,
-                           ohlcv.highs(),
-                           ohlcv.lows(),
-                           period,
-                           outBegIdx,
-                           outNBElement,
-                           output);
+    // Formula:
+    // Aroon Oscillator = Aroon-Up - Aroon-Down
+
+    // compute Aroons
+    final double[] aroonUp = new double[ohlcv.size() - lookback];
+    final double[] aroonDown = new double[aroonUp.length];
+
+    final RetCode outcome = TA_LIB.aroon(start,
+                                         end,
+                                         ohlcv.highs(),
+                                         ohlcv.lows(),
+                                         period,
+                                         outBegIdx,
+                                         outNBElement,
+                                         aroonDown,
+                                         aroonUp);
+    throwExceptionIfBad(outcome, ohlcv);
+
+    // compute indicator
+    for (int i = ZERO; i < output.length; ++i) {
+      output[i] = aroonUp[i] - aroonDown[i];
+    }
+
+    return outcome;
+
+//    return TA_LIB.aroonOsc(start,
+//                           end,
+//                           ohlcv.highs(),
+//                           ohlcv.lows(),
+//                           period,
+//                           outBegIdx,
+//                           outNBElement,
+//                           output);
   }
 
 }
---
+++ src/test/java/org/ikankechil/iota/indicators/trend/AroonOscillatorTest.java
@@ -0,0 +1,33 @@
+/**
+ * AroonOscillatorTest.java v0.1	7 September 2016 12:22:01 am
+ *
+ * Copyright © 2016 Daniel Kuan.  All rights reserved.
+ */
+package org.ikankechil.iota.indicators.trend;
+
+import java.io.IOException;
+
+import org.ikankechil.iota.indicators.AbstractIndicatorTest;
+import org.junit.BeforeClass;
+
+/**
+ * JUnit test for <code>AroonOscillator</code>.
+ * <p>
+ *
+ *
+ * @author Daniel Kuan
+ * @version 0.1
+ */
+public class AroonOscillatorTest extends AbstractIndicatorTest {
+
+  public AroonOscillatorTest() {
+    super(25);
+  }
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws IOException {
+    TEST_CLASS = AroonOscillatorTest.class;
+    AbstractIndicatorTest.setUpBeforeClass();
+  }
+
+}
