2d0409f29a118b974b822b3b7d6bd490ff96d5a0
dkcm <daniel.kuan@gmail.com>
2016-09-20 00:35:07 +0800

    Removed TA-Lib dependency from Ultimate Oscillator.

--- src/main/java/org/ikankechil/iota/indicators/momentum/UltimateOscillator.java
+++ src/main/java/org/ikankechil/iota/indicators/momentum/UltimateOscillator.java
@@ -1,5 +1,5 @@
 /**
- * UltimateOscillator.java  v0.1  4 December 2014 2:28:28 PM
+ * UltimateOscillator.java  v0.2  4 December 2014 2:28:28 PM
  *
  * Copyright © 2014-2016 Daniel Kuan.  All rights reserved.
  */
@@ -18,25 +18,34 @@ import com.tictactec.ta.lib.RetCode;
  * ftp://80.240.216.180/Transmission/%D0%A4%D0%B0%D0%B9%D0%BB%D1%8B/S&C%20on%20DVD%2011.26/VOLUMES/V03/C04/ULTI.pdf
  *
  * @author Daniel Kuan
- * @version
+ * @version 0.2
  */
 public class UltimateOscillator extends AbstractIndicator {
 
-  private final int shortPeriod;
-  private final int mediumPeriod;
-  private final int longPeriod;
+  private final int           shortPeriod;
+  private final int           mediumPeriod;
+  private final int           longPeriod;
+
+  private static final double UO_FACTOR = HUNDRED_PERCENT / (FOUR + TWO + ONE); // 100 / 7
 
   public UltimateOscillator() {
     this(SEVEN, FOURTEEN, TWENTY_EIGHT);
   }
 
   public UltimateOscillator(final int shortPeriod, final int mediumPeriod, final int longPeriod) {
-    super(TA_LIB.ultOscLookback(shortPeriod,
-                                mediumPeriod,
-                                longPeriod));
+    super(longPeriod);
+//    super(TA_LIB.ultOscLookback(shortPeriod,
+//                                mediumPeriod,
+//                                longPeriod));
     throwExceptionIfNegative(shortPeriod,
                              mediumPeriod,
                              longPeriod);
+    if (!(shortPeriod < mediumPeriod && mediumPeriod < longPeriod)) {
+      throw new IllegalArgumentException(String.format("Short: %s, Medium: %s, Long: %s",
+                                                       shortPeriod,
+                                                       mediumPeriod,
+                                                       longPeriod));
+    }
 
     this.shortPeriod = shortPeriod;
     this.mediumPeriod = mediumPeriod;
@@ -60,17 +69,62 @@ public class UltimateOscillator extends AbstractIndicator {
     //
     // Ultimate Oscillator, UO = 100 x [(4 x Average7)+(2 x Average14)+Average28]/(4+2+1)
 
-    return TA_LIB.ultOsc(start,
-                         end,
-                         ohlcv.highs(),
-                         ohlcv.lows(),
-                         ohlcv.closes(),
-                         shortPeriod,
-                         mediumPeriod,
-                         longPeriod,
-                         outBegIdx,
-                         outNBElement,
-                         output);
+    final int size = ohlcv.size();
+    final double[] bp = new double[size - ONE];
+    final double[] tr = new double[bp.length];
+
+    // compute buying pressure and true range
+    int i = ZERO;
+    double pclose = ohlcv.close(i);
+    for (int j = ZERO; ++i < size; ++j) {
+      final double close = ohlcv.close(i);
+      final double min = Math.min(ohlcv.low(i), pclose);
+      bp[j] = close - min;
+      tr[j] = Math.max(ohlcv.high(i), pclose) - min;
+
+      // shift forward in time
+      pclose = close;
+    }
+
+    // compute averages
+    final double[] averageShort = ave(shortPeriod, bp, tr);
+    final double[] averageMedium = ave(mediumPeriod, bp, tr);
+    final double[] averageLong = ave(longPeriod, bp, tr);
+
+    // compute indicator
+    for (int l = ZERO, m = longPeriod - mediumPeriod, s = longPeriod - shortPeriod;
+         l < output.length;
+         ++l, ++m, ++s) {
+      output[l] = UO_FACTOR * ((FOUR * averageShort[s]) + (TWO * averageMedium[m]) + averageLong[l]);
+    }
+
+    outBegIdx.value = lookback;
+    outNBElement.value = output.length;
+    return RetCode.Success;
+
+//    return TA_LIB.ultOsc(start,
+//                         end,
+//                         ohlcv.highs(),
+//                         ohlcv.lows(),
+//                         ohlcv.closes(),
+//                         shortPeriod,
+//                         mediumPeriod,
+//                         longPeriod,
+//                         outBegIdx,
+//                         outNBElement,
+//                         output);
+  }
+
+  private static final double[] ave(final int p, final double[] bp, final double[] tr) {
+    final double[] bpSum = sum(p, bp);
+    final double[] trSum = sum(p, tr);
+
+    final double[] ave = new double[bp.length - p + ONE];
+    for (int i = ZERO; i < ave.length; ++i) {
+      ave[i] = bpSum[i] / trSum[i];
+    }
+
+    return ave;
   }
 
 }
