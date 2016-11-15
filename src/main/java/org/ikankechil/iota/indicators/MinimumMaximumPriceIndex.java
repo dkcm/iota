b5a0eaa0ecfb1d6904897aede6fa5b1609d1ae17
dkcm <daniel.kuan@gmail.com>
2016-11-16 00:32:20 +0800

    Removed TA-Lib dependency from MinimumMaximumPriceIndex.

--- src/main/java/org/ikankechil/iota/indicators/MinimumMaximumPriceIndex.java
+++ src/main/java/org/ikankechil/iota/indicators/MinimumMaximumPriceIndex.java
@@ -1,5 +1,5 @@
 /**
- * MinimumMaximumPriceIndex.java  v0.1  27 January 2015 1:52:20 PM
+ * MinimumMaximumPriceIndex.java  v0.2  27 January 2015 1:52:20 PM
  *
  * Copyright © 2015-2016 Daniel Kuan.  All rights reserved.
  */
@@ -11,51 +11,40 @@ import java.util.List;
 import org.ikankechil.iota.OHLCVTimeSeries;
 import org.ikankechil.iota.TimeSeries;
 
-import com.tictactec.ta.lib.MInteger;
-import com.tictactec.ta.lib.RetCode;
-
 /**
  * Minimum and Maximum Price Index
  *
  *
  * @author Daniel Kuan
- * @version 0.1
+ * @version 0.2
  */
 public class MinimumMaximumPriceIndex extends AbstractIndicator {
 
-  private static final String MININUM_PRICE_INDEX = "Minimum Price Index";
-  private static final String MAXIMUM_PRICE_INDEX = "Maximum Price Index";
+  private final MinimumPriceIndex min;
+  private final MaximumPriceIndex max;
+
+  private static final String     MININUM_PRICE_INDEX = "Minimum Price Index";
+  private static final String     MAXIMUM_PRICE_INDEX = "Maximum Price Index";
 
   public MinimumMaximumPriceIndex(final int period) {
-    super(period, TA_LIB.minMaxIndexLookback(period));
+    super(period, (period - ONE));
+
+    min = new MinimumPriceIndex(period);
+    max = new MaximumPriceIndex(period);
   }
 
   @Override
   public List<TimeSeries> generate(final OHLCVTimeSeries ohlcv) {
     throwExceptionIfShort(ohlcv);
-    final int size = ohlcv.size();
 
-    final MInteger outBegIdx = new MInteger();
-    final MInteger outNBElement = new MInteger();
+    final double[] minPriceIndices = min.generate(ohlcv).get(ZERO).values();
+    final double[] maxPriceIndices = max.generate(ohlcv).get(ZERO).values();
 
-    final int[] outMinIdx = new int[size - lookback];
-    final int[] outMaxIdx = new int[outMinIdx.length];
-
-    final RetCode outcome = TA_LIB.minMaxIndex(ZERO,
-                                               size - ONE,
-                                               ohlcv.closes(),
-                                               period,
-                                               outBegIdx,
-                                               outNBElement,
-                                               outMinIdx,
-                                               outMaxIdx);
-    throwExceptionIfBad(outcome, ohlcv);
-
-    final String[] dates = Arrays.copyOfRange(ohlcv.dates(), lookback, size);
+    final String[] dates = Arrays.copyOfRange(ohlcv.dates(), lookback, ohlcv.size());
 
     logger.info(GENERATED_FOR, name, ohlcv);
-    return Arrays.asList(new TimeSeries(MININUM_PRICE_INDEX, dates, toDoubles(outMinIdx)),
-                         new TimeSeries(MAXIMUM_PRICE_INDEX, dates, toDoubles(outMaxIdx)));
+    return Arrays.asList(new TimeSeries(MININUM_PRICE_INDEX, dates, minPriceIndices),
+                         new TimeSeries(MAXIMUM_PRICE_INDEX, dates, maxPriceIndices));
   }
 
 }
